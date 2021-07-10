495eed5de6c73323e7bfa909b433d0f4de79f866
Alex Lorello <ffseq@protonmail.com>
2021-07-09 22:10:44 -0400

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::convert::TryInto;
 use std::fs;
 
 fn main() {
@@ -9,7 +10,7 @@ fn main() {
     let content = fs::read_to_string(filename)
         .expect("File not found");
     for line in content.lines() {
-        if valid(line) {
+        if valid_part2(line) {
             num_valid_passwords += 1;
         }
     }
@@ -17,7 +18,7 @@ fn main() {
     println!("Number of valid passwords: {}", num_valid_passwords);
 }
 
-fn valid(line: &str) -> bool {
+fn valid_part1(line: &str) -> bool {
 
     let (policy, pass) = split1(line, ':');
     let pass = pass.trim();
@@ -40,7 +41,35 @@ fn valid(line: &str) -> bool {
     range.contains(&count)
 }
 
+fn valid_part2(line: &str) -> bool {
+
+    let (policy, pass) = split1(line, ':');
+    let pass = pass.trim();
+    let (policy_range, policy_char) = split1(policy, ' ');
+    let policy_char: char = policy_char.parse().unwrap();
+    let (low, high) = split1(policy_range, '-');
+
+    let first = low.parse::<i32>().unwrap() - 1;
+    let second = high.parse::<i32>().unwrap() - 1;
+
+    if at(pass, first) == policy_char {
+        return at(pass, second) != policy_char;
+    }
+    else if at(pass, second) == policy_char {
+        return at(pass, first) != policy_char;
+    }
+
+    else {
+        return false
+    }
+        
+}
+
 fn split1(input: &str, c: char) -> (&str, &str) {
     let splitted = input.splitn(2, c).collect::<Vec<&str>>();
     (splitted[0], splitted[1])
+}
+
+fn at(string: &str, idx: i32) -> char {
+    string.chars().nth(idx.try_into().unwrap()).unwrap()
 }
